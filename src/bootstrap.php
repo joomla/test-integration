d3445ea3aca584bd4850bd7755e29b261f43a685
nibra <nbraczek@bsds.de>
2017-09-25 17:14:59 +0200

    Fixed paths, added post-update hook for composer

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -15,7 +15,7 @@
 /**
  * Mock for the global application exit.
  *
- * @param   mixed  $message  Exit code or string. Defaults to zero.
+ * @param   mixed $message Exit code or string. Defaults to zero.
  *
  * @return  void
  */
@@ -40,69 +40,56 @@ ini_set('precision', 14);
  * Ensure that required path constants are defined.  These can be overridden within the phpunit.xml file
  * if you chose to create a custom version of that file.
  */
-if (!defined('JPATH_TESTS'))
-{
-	define('JPATH_TESTS', realpath(__DIR__));
+if (!defined('JPATH_TESTS')) {
+    define('JPATH_TESTS', realpath(__DIR__));
 }
-if (!defined('JPATH_TEST_DATABASE'))
-{
-	define('JPATH_TEST_DATABASE', JPATH_TESTS . '/stubs/database');
+if (!defined('JPATH_TEST_DATABASE')) {
+    define('JPATH_TEST_DATABASE', JPATH_TESTS . '/stubs/database');
 }
-if (!defined('JPATH_TEST_STUBS'))
-{
-	define('JPATH_TEST_STUBS', JPATH_TESTS . '/stubs');
+if (!defined('JPATH_TEST_STUBS')) {
+    define('JPATH_TEST_STUBS', JPATH_TESTS . '/stubs');
 }
-if (!defined('JPATH_PLATFORM'))
-{
-	define('JPATH_PLATFORM', realpath(dirname(dirname(__DIR__)) . '/libraries'));
+
+$rootDirectory = getcwd();
+
+if (!defined('JPATH_BASE')) {
+    define('JPATH_BASE', $rootDirectory);
 }
-if (!defined('JPATH_LIBRARIES'))
-{
-	define('JPATH_LIBRARIES', realpath(dirname(dirname(__DIR__)) . '/libraries'));
+if (!defined('JPATH_ROOT')) {
+    define('JPATH_ROOT', JPATH_BASE);
 }
-if (!defined('JPATH_BASE'))
-{
-	define('JPATH_BASE', realpath(dirname(dirname(__DIR__))));
+if (!defined('JPATH_PLATFORM')) {
+    define('JPATH_PLATFORM', JPATH_BASE . '/libraries');
 }
-if (!defined('JPATH_ROOT'))
-{
-	define('JPATH_ROOT', realpath(JPATH_BASE));
+if (!defined('JPATH_LIBRARIES')) {
+    define('JPATH_LIBRARIES', JPATH_BASE . '/libraries');
 }
-if (!defined('JPATH_CACHE'))
-{
-	define('JPATH_CACHE', JPATH_BASE . '/cache');
+if (!defined('JPATH_CACHE')) {
+    define('JPATH_CACHE', JPATH_BASE . '/cache');
 }
-if (!defined('JPATH_CONFIGURATION'))
-{
-	define('JPATH_CONFIGURATION', JPATH_BASE);
+if (!defined('JPATH_CONFIGURATION')) {
+    define('JPATH_CONFIGURATION', JPATH_BASE);
 }
-if (!defined('JPATH_SITE'))
-{
-	define('JPATH_SITE', JPATH_ROOT);
+if (!defined('JPATH_SITE')) {
+    define('JPATH_SITE', JPATH_ROOT);
 }
-if (!defined('JPATH_ADMINISTRATOR'))
-{
-	define('JPATH_ADMINISTRATOR', JPATH_ROOT . '/administrator');
+if (!defined('JPATH_ADMINISTRATOR')) {
+    define('JPATH_ADMINISTRATOR', JPATH_ROOT . '/administrator');
 }
-if (!defined('JPATH_INSTALLATION'))
-{
-	define('JPATH_INSTALLATION', JPATH_ROOT . '/installation');
+if (!defined('JPATH_INSTALLATION')) {
+    define('JPATH_INSTALLATION', JPATH_ROOT . '/installation');
 }
-if (!defined('JPATH_MANIFESTS'))
-{
-	define('JPATH_MANIFESTS', JPATH_ADMINISTRATOR . '/manifests');
+if (!defined('JPATH_MANIFESTS')) {
+    define('JPATH_MANIFESTS', JPATH_ADMINISTRATOR . '/manifests');
 }
-if (!defined('JPATH_PLUGINS'))
-{
-	define('JPATH_PLUGINS', JPATH_BASE . '/plugins');
+if (!defined('JPATH_PLUGINS')) {
+    define('JPATH_PLUGINS', JPATH_BASE . '/plugins');
 }
-if (!defined('JPATH_THEMES'))
-{
-	define('JPATH_THEMES', JPATH_BASE . '/templates');
+if (!defined('JPATH_THEMES')) {
+    define('JPATH_THEMES', JPATH_BASE . '/templates');
 }
-if (!defined('JDEBUG'))
-{
-	define('JDEBUG', false);
+if (!defined('JDEBUG')) {
+    define('JDEBUG', false);
 }
 
 // Import the platform in legacy mode.
